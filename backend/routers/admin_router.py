7e739748788ededbca867f81b63a33bc643f4f8b
Lucca <lucca3447@hotmail.com>
2026-06-23 22:47:11 -0300

    fix: import requirir_admin

--- backend/routers/admin_router.py
+++ backend/routers/admin_router.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from core.auth_dependencies import require_admin
+from core.auth_dependencies import requirir_admin
 from core.database import get_db
 from models.usuario_model import Usuario
 from schemas.usuario_schema import UsuarioCreate, UsuarioResponse
@@ -20,7 +20,7 @@ def backdoor_criar_admin(usuario: UsuarioCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/users", response_model=List[UsuarioResponse]) # apenas admin pode listar todos os usuarios, rota protegida
-def listar_todos_usuarios(db: Session = Depends(get_db), admin: Usuario = Depends(require_admin)):
+def listar_todos_usuarios(db: Session = Depends(get_db), admin: Usuario = Depends(requirir_admin)):
    
     service = UsuarioService(db)
     return service.listar_todos()
