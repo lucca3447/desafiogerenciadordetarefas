46ebf8104d6ba85698a5b272c7c9844ee37629f6
Lucca <lucca3447@hotmail.com>
2026-06-28 01:12:17 -0300

    feat/ botao de logout no dashboard

--- frontend/frontend-task-manager/src/pages/Dashboard.jsx
+++ frontend/frontend-task-manager/src/pages/Dashboard.jsx
@@ -1,7 +1,18 @@
+import { useAuth } from "../context/AuthContext";
+
 export default function Dashboard() {
+
+  const {user, logout} = useAuth();
+
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-50">
-      <h1 className="text-2xl font-bold text-gray-800">Dashboard sem nada por enquanto </h1>
+    <div className="flex h-screen items-center justify-center bg-gray-50 flex-col gap-6">
+      <h1 className="text-2xl font-bold text-gray-800"> 
+        Eai, {user?.nome}! </h1>
+        
+      {/* botao de logout */}
+      <button onClick={logout} className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-6 rounded-lg transition-colors">
+        Sair da conta
+      </button>
     </div>
   );
 }
